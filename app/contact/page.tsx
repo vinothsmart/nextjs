e74840afba72f000be879b1dccffbf9176985809
vinothsmart <s.vinothkanna90@gmail.com>
2025-07-23 19:56:30 +0530

    onclick need use client

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,11 @@
+"use client";
 const Contact = () => {
   console.log("Hey is this in the server or client?");
-  return <div>Contact Page</div>;
+  return (
+    <div>
+      <button onClick={() => alert("Button clicked!")}>Button Click</button>
+    </div>
+  );
 };
 
 export default Contact;
