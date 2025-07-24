910f355280d7ba50ad90c63e63357672b2afd5af
vinothsmart <s.vinothkanna90@gmail.com>
2025-07-24 19:54:24 +0530

    data fetching

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,8 +1,31 @@
 "use client";
+import React, { useEffect, useState } from "react";
+
 const Contact = () => {
-  console.log("Hey is this in the server or client?");
+  const [data, setData] = useState<{ title: string; body: string } | null>(
+    null
+  );
+
+  const fetchData = async () => {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/posts/1"
+    );
+    const result = await response.json();
+    setData(result);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
+      <h1>{data.title}</h1>
+      <p>{data.body}</p>
       <button onClick={() => alert("Button clicked!")}>Button Click</button>
     </div>
   );
