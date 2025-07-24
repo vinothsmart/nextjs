4e77471c5ce7bfe74e3c2dc7571bb8be48a16672
vinothsmart <s.vinothkanna90@gmail.com>
2025-07-24 20:02:26 +0530

    loading data async func

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,32 +1,10 @@
-"use client";
-import React, { useEffect, useState } from "react";
-
-const Contact = () => {
-  const [data, setData] = useState<{ title: string; body: string } | null>(
-    null
-  );
-
-  const fetchData = async () => {
-    const response = await fetch(
-      "https://jsonplaceholder.typicode.com/posts/1"
-    );
-    const result = await response.json();
-    setData(result);
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  if (!data) {
-    return <div>Loading...</div>;
-  }
-
+const Contact = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const result = await response.json();
+  console.log(result);
   return (
     <div>
-      <h1>{data.title}</h1>
-      <p>{data.body}</p>
-      <button onClick={() => alert("Button clicked!")}>Button Click</button>
+      {/* <button onClick={() => alert("Button clicked!")}>Button Click</button> */}
     </div>
   );
 };
