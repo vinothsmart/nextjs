32611e3db1f31ecfea3518b7706b4783c973e793
vinothsmart <s.vinothkanna90@gmail.com>
2025-07-28 19:45:06 +0530

    dynamic user

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,10 +1,31 @@
+const fetchUser = async (userId: string) => {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${userId}`
+  );
+  if (!response.ok) {
+    throw new Error("User not found");
+  }
+  return response.json();
+};
+
 const UserPage = async ({
   params,
 }: {
   params: Promise<{ userId: string }>;
 }) => {
   const { userId } = await params;
-  return <div>User ID: {userId}</div>;
+  const user = await fetchUser(userId);
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">User Details</h1>
+      <div className="mt-4">
+        <p className="text-lg">Name: {user.name}</p>
+        <p>Email: {user.email}</p>
+        <p>Phone: {user.phone}</p>
+        <p>Website: {user.website}</p>
+      </div>
+    </div>
+  );
 };
 
 export default UserPage;
