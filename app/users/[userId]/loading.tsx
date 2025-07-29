e2519a89b96d9d63564061b9c9360316c37a011f
vinothsmart <s.vinothkanna90@gmail.com>
2025-07-29 10:39:07 +0530

    loader up

--- app/users/[userId]/loading.tsx
+++ app/users/[userId]/loading.tsx
@@ -1,4 +1,11 @@
 const LoadingState = () => {
-  return <div>loading</div>;
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="flex flex-col items-center space-y-4">
+        <div className="w-12 h-12 border-4 border-blue-400 rounded-full animate-spin border-t-transparent"></div>
+        <p className="text-xl text-gray-700 font-semibold">Loading...</p>
+      </div>
+    </div>
+  );
 };
 export default LoadingState;
