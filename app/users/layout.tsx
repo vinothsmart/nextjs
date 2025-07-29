99e5b3b1a2ed7eb302223d569036123622f5d8c0
vinothsmart <s.vinothkanna90@gmail.com>
2025-07-29 10:51:44 +0530

    arrow syntax diff

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,12 +1,11 @@
-export default function UserLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+"use client";
+
+const UserLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
       <h1 className="text-2xl font-bold mb-4">User Section</h1>
       {children}
     </div>
   );
-}
+};
+export default UserLayout;
