bc44528dfb33528e81b50f451a4a583b844fbcca
vinothsmart <s.vinothkanna90@gmail.com>
2025-07-29 14:58:17 +0530

    completed

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,16 @@
-"use client";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About Page",
+  description: "A lot of keywords",
+  keywords: ["about", "page", "next.js"],
+  twitter: {
+    card: "summary_large_image",
+    site: "@yourtwitterhandle",
+    title: "About Page",
+    description: "A lot of keywords",
+  },
+};
 
 const About = () => {
   console.log("Hey is this in the server or client?");
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,4 +1,17 @@
+import { Metadata } from "next";
 import Button from "./button";
+
+export const metadata: Metadata = {
+  title: "Contact Page",
+  description: "A lot of keywords",
+  keywords: ["contact", "page", "next.js"],
+  twitter: {
+    card: "summary_large_image",
+    site: "@yourtwitterhandle",
+    title: "Contact Page",
+    description: "A lot of keywords",
+  },
+};
 
 const Contact = async () => {
   const response = await fetch("https://jsonplaceholder.typicode.com/posts");
