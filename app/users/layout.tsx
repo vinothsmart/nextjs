88222d070e6ec9e7be2466d8c3111a5ca09c8a14
vinothsmart <s.vinothkanna90@gmail.com>
2025-07-29 10:50:34 +0530

    Common Layout

---
+++ app/users/layout.tsx
@@ -0,0 +1,12 @@
+export default function UserLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">User Section</h1>
+      {children}
+    </div>
+  );
+}
